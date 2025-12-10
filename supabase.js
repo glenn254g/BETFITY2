de0cd14e965fede623ba3a9b80081e9856d53086
glenn254g <glennnyamweya3@gmail.com>
2025-12-10 22:41:05 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // Supabase client connection
-const supabaseUrl = "YOUR_PROJECT_URL";
-const supabaseKey = "YOUR_ANON_PUBLIC_KEY";
+const supabaseUrl = "https://curktrkcwobraducbkzj.supabase.co";
+const supabaseKey = "sb_publishable_tTjEWEJtcNYqK95wamq3cw_nkLdW-A2";
 
 window.supabase = supabase.createClient(supabaseUrl, supabaseKey);
