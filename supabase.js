f52f30a27f3ca548366d5fdd0c1b18f7f9533f1f
glenn254g <glennnyamweya3@gmail.com>
2025-12-10 22:36:04 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,5 @@
+// Supabase client connection
+const supabaseUrl = "YOUR_PROJECT_URL";
+const supabaseKey = "YOUR_ANON_PUBLIC_KEY";
 
+window.supabase = supabase.createClient(supabaseUrl, supabaseKey);
